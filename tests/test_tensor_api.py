f7e8b1f0c845f180f1fdb452b7ca88563f7b6be7
benedictclarkson1 <benedictclarkson1@gmail.com>
2024-01-14 17:14:08 +0000

    Fixed broken test

--- tests/test_tensor_api.py
+++ tests/test_tensor_api.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 import numpy as np
 
 from tricycle_v2.binary import badd, bdiv, bmul, bsub
@@ -13,6 +15,11 @@ def test_can_add_tensors():
 
     assert np.allclose(tensor_1 + tensor_2, badd(tensor_1, tensor_2))
 
+    before = deepcopy(tensor_1)
+    tensor_1 += 1
+
+    assert np.allclose(tensor_1, uadd(before, 1))
+
 
 def test_can_subtract_tensors():
     tensor_1 = to_tensor(np.arange(12).reshape(3, 4))
@@ -21,6 +28,11 @@ def test_can_subtract_tensors():
     assert np.allclose(tensor_1 - 1, usub(tensor_1, 1))
 
     assert np.allclose(tensor_1 - tensor_2, bsub(tensor_1, tensor_2))
+
+    before = deepcopy(tensor_1)
+    tensor_1 -= 1
+
+    assert np.allclose(tensor_1, usub(before, 1))
 
 
 def test_can_multiply_tensors():
@@ -31,14 +43,24 @@ def test_can_multiply_tensors():
 
     assert np.allclose(tensor_1 * tensor_2, bmul(tensor_1, tensor_2))
 
+    before = deepcopy(tensor_1)
+    tensor_1 *= 2
+
+    assert np.allclose(tensor_1, umul(before, 2))
+
 
 def test_can_divide_tensors():
-    tensor_1 = to_tensor(np.arange(1, 13).reshape(3, 4))
-    tensor_2 = to_tensor(np.arange(1, 13).reshape(3, 4))
+    tensor_1 = to_tensor(np.arange(1, 13).reshape(3, 4).astype(float))
+    tensor_2 = to_tensor(np.arange(1, 13).reshape(3, 4).astype(float))
 
     assert np.allclose(tensor_1 / 2, udiv(tensor_1, 2))
 
     assert np.allclose(tensor_1 / tensor_2, bdiv(tensor_1, tensor_2))
+
+    before = deepcopy(tensor_1)
+    tensor_1 /= 2.0
+
+    assert np.allclose(tensor_1, udiv(before, 2))
 
 
 def test_can_pow_tensors():
