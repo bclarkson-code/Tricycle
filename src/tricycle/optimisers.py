fbf05e5303773f16c398fc2d8dd390d2494de3f4
benedictclarkson1 <benedictclarkson1@gmail.com>
2024-01-28 15:25:58 +0000

    Added momentum

--- src/tricycle/optimisers.py
+++ src/tricycle/optimisers.py
@@ -1,4 +1,6 @@
-from tricycle.tensor import Tensor
+import numpy as np
+
+from tricycle.tensor import Tensor, to_tensor
 
 
 class Optimiser:
@@ -7,9 +9,17 @@ class Optimiser:
 
 
 class StochasticGradientDescent:
-    def __init__(self, learning_rate: float, weight_decay: float | None = None):
+    def __init__(
+        self,
+        learning_rate: float,
+        weight_decay: float | None = None,
+        momentum: float | None = None,
+    ):
         self.learning_rate = learning_rate
         self.weight_decay = weight_decay
+        self.momentum = momentum
+
+        self.momentum_store = {}
 
     def _reset_grad(self, tensor: Tensor):
         tensor.grad = None
@@ -18,14 +28,33 @@ class StochasticGradientDescent:
         return tensor
 
     def update_weight(self, tensor: Tensor):
+        """
+        Perform a gradient update on a tensor, optionally
+        including weight decay and momentum
+        """
         assert tensor.grad is not None
 
         grad = self.learning_rate * tensor.grad
-        if self.weight_decay is None:
-            return tensor - grad
 
-        wd = self.learning_rate * self.weight_decay * tensor
-        return tensor - wd - grad
+        if self.weight_decay is not None:
+            wd = self.learning_rate * self.weight_decay * tensor
+            grad += wd
+
+        if self.momentum is not None:
+            if tensor.uuid not in self.momentum_store:
+                last_momentum = to_tensor(np.zeros_like(grad))
+            else:
+                last_momentum = self.momentum_store[tensor.uuid]
+
+            grad += self.momentum * last_momentum
+            self.momentum_store[tensor.uuid] = grad
+
+        # We need to make sure that the new tensor looks like the old one
+        old_uuid = tensor.uuid
+        result = tensor - grad
+        result.uuid = old_uuid
+
+        return result
 
     def __call__(self, tensor: Tensor) -> Tensor:
         return self._reset_grad(self.update_weight(tensor))
--- tests/test_optimisers.py
+++ tests/test_optimisers.py
@@ -52,9 +52,10 @@ def test_can_train_simple_neural_network_no_wd():
 
     assert losses[-1] < 1.5
 
+
 def test_can_train_simple_neural_network_wd():
     """
-    Train a simple neural network on the iris dataset
+    Train a simple neural network on the iris dataset with weight decay
     """
     np.random.seed(42)
     X, y = load_iris(return_X_y=True)
@@ -92,3 +93,48 @@ def test_can_train_simple_neural_network_wd():
             model.zero_grad()
 
     assert losses[-1] < 1.1
+
+
+def test_can_train_simple_neural_network_momentum():
+    """
+    Train a simple neural network on the iris dataset with momentum
+    """
+    np.random.seed(42)
+    X, y = load_iris(return_X_y=True)
+    # one hot encode y
+    y = np.eye(3)[y.astype(int)]
+
+    # create a dataset
+    ds = Dataset(X, y)
+
+    # create a model
+    layer_1 = Dense(4, 16)
+    layer_2 = Dense(16, 3)
+    relu = ReLU()
+    model = Sequential(layer_1, relu, layer_2).vectorise()
+    loss_fn = CrossEntropy().vectorise()
+    optimiser = StochasticGradientDescent(learning_rate=1e-2, momentum=0.9)
+
+    BATCH_SIZE = 16
+    N_EPOCHS = 10
+
+    losses = []
+    for _ in range(N_EPOCHS):
+        batches = ds.copy().to_tensor().shuffle().batch(BATCH_SIZE)
+        for x, y in batches:
+            x = to_tensor(x)
+            y = to_tensor(y)
+
+            y_pred = model(x)
+            loss = loss_fn(y, y_pred)
+            loss = radd(loss / len(x), "i->")
+            loss.backward()
+            losses.append(loss)
+
+            model.update(optimiser)
+            model.zero_grad()
+
+            assert layer_1.weights.uuid in optimiser.momentum_store
+            assert layer_2.weights.uuid in optimiser.momentum_store
+
+    assert losses[-1] < 0.3
