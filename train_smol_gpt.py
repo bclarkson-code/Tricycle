28d061ac01014fa2606f44852db3a1452a460a06
bclarkson-code <57139598+bclarkson-code@users.noreply.github.com>
2024-04-01 18:26:17 +0100

    Train transformer (#28)
    * Added first draft of MLP block
    
    * Implemented dropout
    
    * Fixed bug with tensor API
    
    * Added MLP block
    
    * Fixed bug in tensor
    
    * Added mean and standard deviation
    
    * Added first draft of layer norm
    
    * First draft of a smol GPT
    
    * Renamed models
    
    * Added byte pair encoding tokeniser
    
    * Started adding shakespeare dataset
    
    * Added first draft of shakespeare
    
    * Added shakespeare generation script
    
    * Added generated scripts to gitignore
    
    * Built causallm dataset
    
    * First draft of trainnig script
    
    * Added VERY slow training script
    
    * Reduced size of config
    
    * Train for a single batch
    
    * Fixed broken test
    
    * Updated default config
    
    * Cleaned up trainign script
    
    * Updated README

--- train_smol_gpt.py
+++ train_smol_gpt.py
@@ -5,7 +5,7 @@ works of shakespeare.
 The hyperparams for this model are very much a work in progress
 """
 
-import os
+import pickle
 
 from tqdm import tqdm
 
@@ -18,14 +18,6 @@ from tricycle_datasets.shakespeare import Shakespeare
 
 config = SmolGPTConfig()
 model = GPT(config)
-
-if config.mlflow_enabled:
-    import mlflow
-
-    mlflow.set_tracking_uri(config.mlflow_tracking_uri)
-    mlflow.set_experiment(config.mlflow_experiment_name)
-    os.environ["MLFLOW_ENABLE_SYSTEM_METRICS_LOGGING"] = "true"
-
 
 tokens = Shakespeare(vocab_size=config.vocab_size)
 dataset = (
@@ -46,30 +38,15 @@ optimiser = StochasticGradientDescent(
     momentum=config.momentum,
 )
 
-if config.mlflow_enabled:
-    with mlflow.start_run():
-        mlflow.log_params(config.__dict__)
+for inputs, outputs in tqdm(dataset):
+    logits = model(inputs)
+    loss = loss_fn(outputs, logits).from_vector().mean().mean()
+    loss.backward()
+    model.update(optimiser)
 
-        for step, (inputs, outputs) in tqdm(
-            enumerate(dataset), total=len(dataset)
-        ):
-            if step > 100:
-                break
-            logits = model(inputs)
-            loss = loss_fn(outputs, logits).from_vector().mean().mean()
-            loss.backward()
-            model.update(optimiser)
-            mlflow.log_metric("loss", loss, step=step)
-else:
-    for step, (inputs, outputs) in tqdm(
-        enumerate(dataset), total=len(dataset)
-    ):
-        if step > 10:
-            break
-        logits = model(inputs)
-        loss = loss_fn(outputs, logits).from_vector().mean().mean()
-        loss.backward()
-        model.update(optimiser)
+    # clean up the computational graph
+    loss.cleanup()
 
-        # important: avoids memory leak
-        loss.cleanup()
+# save results
+with open("model.pkl", "wb") as f:
+    pickle.dump(model, f)
