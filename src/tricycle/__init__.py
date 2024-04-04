5a8193c496d0ca73d84c0f8bf11be0a3e08411d0
Clarkson, Benedict <benedict.clarkson@sky.uk>
2024-04-04 16:48:48 +0100

    Checked that we can actually use cupy

--- src/tricycle/__init__.py
+++ src/tricycle/__init__.py
@@ -3,7 +3,10 @@ from warnings import warn
 try:
     import cupy
 
+    cupy.array([1, 2, 3])
     CUPY_ENABLED = True
 except ImportError:
     warn("Could not find CuPY, disabling GPU features")
     CUPY_ENABLED = False
+except Exception:
+    warn("Failed to build cupy array. Disabling GPU features")
