1477d9418656230fe786020f9db35168ec7b0257
ben <benedictclarkson1@gmail.com>
2024-05-05 09:53:52 +0100

    Cleaned up config

--- src/tricycle/configs.py
+++ src/tricycle/configs.py
@@ -9,24 +9,30 @@ class GPTConfig:
     n_heads: int
     n_layers: int
     expansion_ratio: float
-    activation_fn: Layer
+    activation_fn: float
 
     input_dropout_prob: float
     attention_dropout_prob: float
     residual_dropout_prob: float
     linear_dropout_prob: float
 
-    learning_rate: float
+    max_learning_rate: float
+    min_learning_rate: float
+    warmup_steps: int
     weight_decay: float
-    momentum: float
+    momentum = float
+    beta1: float
+    beta2: float
 
+    steps: int
+    eval_interval: int
     batch_size: int
+    gradient_accumulation_steps: int
 
-    mlflow_enabled: bool
+    device_idx: int
+
     mlflow_tracking_uri: str
     mlflow_experiment_name: str
-
-    pad_token_id: int
 
 
 class SmolGPTConfig(GPTConfig):
@@ -36,7 +42,7 @@ class SmolGPTConfig(GPTConfig):
     n_heads = 6
     n_layers = 6
     expansion_ratio = 4
-    activation_fn = GeLU()
+    activation_fn = "gelu"
 
     input_dropout_prob = 0
     attention_dropout_prob = 0
@@ -46,19 +52,17 @@ class SmolGPTConfig(GPTConfig):
     max_learning_rate = 1e-3
     min_learning_rate = 1e-4
     warmup_steps = 100
-    steps = 5_000
     weight_decay = 1e-1
     momentum = 0
-
     beta1 = 0.9
     beta2 = 0.99
+
+    steps = 5_000
+    eval_interval = 50
+    batch_size = 16
     gradient_accumulation_steps = 1
 
-    batch_size = 16
+    device_idx = 0
 
-    debug = True
     mlflow_enabled = True
     mlflow_tracking_uri = "http://localhost:5000"
-    mlflow_experiment_name = "Tricycle SmolGPT"
-
-    pad_token_id = 0
