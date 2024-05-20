a430dfa05bca7d555615d252e83aa94691ceff81
ben <benedictclarkson1@gmail.com>
2024-05-20 20:21:23 +0100

    dramatically improved optimiser performance

--- src/tricycle/optimisers.py
+++ src/tricycle/optimisers.py
@@ -1,6 +1,8 @@
+import cupy as cp
 import numpy as np
 
 from tricycle.tensor import Tensor, to_tensor
+from tricycle.utils import log_memory_and_time
 
 
 class Optimiser:
@@ -110,31 +112,117 @@ class AdamW(Optimiser):
         )
 
         self.v[key] = self.betas[1] * self.v[key] + (1 - self.betas[1]) * (
-            tensor.grad._data**2
+            tensor.grad._data * tensor.grad._data
         )
 
         m_hat = self.m[key] / (1 - self.betas[0] ** self.t)
         v_hat = self.v[key] / (1 - self.betas[1] ** self.t)
 
-        result = tensor._data - self.learning_rate * (
+        tensor._data -= self.learning_rate * (
             m_hat / (xp.sqrt(v_hat) + self.eps)
             + self.weight_decay * tensor._data
         )
-        result = to_tensor(
-            result,
-            requires_grad=tensor.requires_grad,
-            name=tensor.name,
-            is_vector=tensor.is_vector,
-            _id=tensor._id,
-        )
 
-        assert result.shape == tensor.shape
+        tensor.grad._data.fill(0)
 
-        # not sure if we need to do this
-        del tensor.grad
-        del tensor
-
-        return result
+        return tensor
 
     def __call__(self, tensor: Tensor) -> Tensor:
         return self._reset_grad(self.update_weight(tensor))
+
+
+# @cp.fuse()
+def _calculate_momentum(
+    beta_0: float, prev_momentum: cp.ndarray, grad: cp.ndarray
+) -> cp.ndarray:
+    return beta_0 * prev_momentum + (1 - beta_0) * grad
+
+
+# @cp.fuse()
+def _calculate_v(
+    beta_1: float, prev_v: cp.ndarray, grad: cp.ndarray
+) -> cp.ndarray:
+    return beta_1 * prev_v + (1 - beta_1) * (grad**2)
+
+
+# @cp.fuse()
+def _estimate_param(beta: float, val: cp.ndarray, t: float) -> cp.ndarray:
+    return val / (1 - beta**t)
+
+
+# @cp.fuse()
+def _update(
+    tensor: cp.ndarray,
+    learning_rate: float,
+    m_hat: cp.ndarray,
+    v_hat: cp.ndarray,
+    eps: float,
+    weight_decay: float,
+):
+    """
+    Inplace update tensor
+    """
+    coef = m_hat / (cp.sqrt(v_hat) + eps)
+    return learning_rate * coef + weight_decay * tensor
+
+
+class AdamWV2(Optimiser):
+    def __init__(
+        self,
+        learning_rate=1e-3,
+        betas=(0.9, 0.999),
+        eps=1e-8,
+        weight_decay=0.01,
+    ):
+        self.learning_rate = learning_rate
+        self.betas = betas
+        self.eps = eps
+        self.weight_decay = weight_decay
+        self.t = 0
+
+        self.m = {}
+        self.v = {}
+
+    def step(self):
+        # we compute the updates dynamically so we'll need to remember to
+        # call this
+        self.t += 1
+
+    def update_weight(self, tensor: Tensor) -> Tensor:
+        key = tensor._id
+        xp = tensor.xp
+
+        # initialise stores
+        if key not in self.m:
+            self.m[key] = xp.zeros_like(tensor._data)
+        if key not in self.v:
+            self.v[key] = tensor.xp.zeros_like(tensor._data)
+
+        self.m[key] = _calculate_momentum(
+            self.betas[0], self.m[key], tensor.grad._data
+        )
+
+        self.v[key] = _calculate_v(
+            self.betas[1], self.v[key], tensor.grad._data
+        )
+
+        m_hat = _estimate_param(self.betas[0], self.m[key], self.t)
+        v_hat = _estimate_param(self.betas[1], self.v[key], self.t)
+
+        # this update is don inplace to avoid synchronisation
+        tensor._data -= _update(
+            tensor._data,
+            self.learning_rate,
+            m_hat,
+            v_hat,
+            self.eps,
+            self.weight_decay,
+        )
+        return tensor
+
+    def __call__(self, tensor: Tensor) -> Tensor:
+        tensor = self.update_weight(tensor)
+        # log_memory_and_time("update_weight")
+        tensor = self._reset_grad(tensor)
+        # log_memory_and_time("reset_grad")
+        return tensor
