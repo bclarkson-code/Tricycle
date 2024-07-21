807ef37670be37adff358be63d60e7baabbf1861
ben <benedictclarkson1@gmail.com>
2024-07-21 14:39:17 +0100

    Added tests for new kernels

--- tests/test_activations.py
+++ tests/test_activations.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from tricycle.activation import GLU, GeLU, ReLU, Swish
+from tricycle import GPU_ENABLED
+from tricycle.activation import GLU, CudaGeLU, CudaReLU, GeLU, ReLU, Swish
 from tricycle.tensor import Tensor
 
 
@@ -55,3 +56,48 @@ def test_glu():
 
     y = glu(x)
     assert y.close_to([0.73105858, 0.73105858, 0.73105858], rtol=1e-3)
+
+
+if GPU_ENABLED:
+
+    def test_relus_match():
+        np.random.seed(0)
+        INPUT_SHAPE = (32, int(2**15))
+        random_data = np.random.random(INPUT_SHAPE) * 2 - 1
+        tensor_1 = Tensor(random_data.copy())
+        tensor_2 = Tensor(random_data.copy())
+
+        tensor_1.to_gpu()
+        tensor_2.to_gpu()
+
+        relu = ReLU()
+        output_1 = relu(tensor_1)
+        output_1.backward()
+
+        cuda_relu = CudaReLU()
+        output_2 = cuda_relu(tensor_2)
+        output_2.backward()
+
+        assert output_1.close_to(output_2)
+        assert tensor_1.grad.close_to(tensor_2.grad)
+
+    def test_gelus_match():
+        np.random.seed(0)
+        INPUT_SHAPE = (32, int(2**15))
+        random_data = np.random.random(INPUT_SHAPE) * 2 - 1
+        tensor_1 = Tensor(random_data.copy())
+        tensor_2 = Tensor(random_data.copy())
+
+        tensor_1.to_gpu()
+        tensor_2.to_gpu()
+
+        relu = GeLU()
+        output_1 = relu(tensor_1)
+        output_1.backward()
+
+        cuda_relu = CudaGeLU()
+        output_2 = cuda_relu(tensor_2)
+        output_2.backward()
+
+        assert output_1.close_to(output_2)
+        assert tensor_1.grad.close_to(tensor_2.grad, rtol=1e-5, atol=1e-6)
